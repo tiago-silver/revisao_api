28157afd73aaa03a74f06b9272539580295b7107
tiago-silver <tiagopereirasilva5425@gmail.com>
2024-09-30 20:33:48 -0300

    Projeto finalizado

--- src/controller/tagsController.js
+++ src/controller/tagsController.js
@@ -7,7 +7,9 @@ class TagsController {
         // Mudar a forma de obter a o id
         const user_id = request.user.id
 
-        const tags = await knex("tags").where({user_id})
+        const tags = await knex("tags")
+        .where({user_id})
+        .groupBy("name")
 
         return response.json(tags)
     }
