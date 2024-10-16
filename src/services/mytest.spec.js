1f0419abce2846bb232b4440e86ab562efae495c
tiago-silver <tiagopereirasilva5425@gmail.com>
2024-10-16 14:57:46 -0300

    finalizado testes automatizados para cadastro de usuários

--- src/services/mytest.spec.js
+++ src/services/mytest.spec.js
@@ -2,10 +2,19 @@
 const UserCreateServices = require("./UserCreateServices")
 // importação do dados em memória
 const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory")
+const AppError = require("../utils/AppError")
 
 
 // Utiliza o describe para separar os testes, por exemplo : criação de usuário e criação de notas
 describe("UserCreateServices", ()=> {
+    let userRepositoryInMemory = null
+    let userCreateServices = null
+
+
+    beforeEach(()=>{
+        userRepositoryInMemory = new UserRepositoryInMemory()
+        userCreateServices = new UserCreateServices(userRepositoryInMemory)
+    })
 
     it("Se o usuário será criado",async()=>{
     
@@ -16,13 +25,28 @@ describe("UserCreateServices", ()=> {
             password : "123"
         }
         // Para simular o banco de dados original, utiliza dados em memória
-        const userRepositoryInMemory = new UserRepositoryInMemory()
-        const userCreateServices = new UserCreateServices(userRepositoryInMemory)
         const userCreated = await userCreateServices.execute(user)
-    
-    
-        console.log(userCreated)
+       
         expect(userCreated).toHaveProperty("id")
+    })
+
+    it("O usuário não pode cadastrar com um email já existente!",async () => {
+        const user1 = {
+            name: "User Test 1",
+            email : "user@test.com",
+            password : "123"
+        }
+        const user2 = {
+            name: "User Test 2",
+            email : "user@test.com",
+            password : "123"
+        }
+
+      
+
+        await userCreateServices.execute(user1)
+        await expect(userCreateServices.execute(user2)).rejects.toEqual(new AppError("Este email já existe!"))
+
     })
 
 })
